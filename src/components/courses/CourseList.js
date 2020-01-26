2a0b9c3a0bef57fa0645da6318ed9e4cf33198b3
David Yaari <david@pronobia.com>
2020-01-26 15:57:42 +0200

    Async writes

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -1,9 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const CourseList = ({ courses }) => (
-  <table className="table">
+  <table className='table'>
     <thead>
       <tr>
         <th />
@@ -18,19 +18,19 @@ const CourseList = ({ courses }) => (
           <tr key={course.id}>
             <td>
               <a
-                className="btn btn-light"
-                href={"http://pluralsight.com/courses/" + course.slug}
+                className='btn btn-light'
+                href={'http://pluralsight.com/courses/' + course.slug}
               >
                 Watch
               </a>
             </td>
             <td>
-              <Link to={"/course/" + course.slug}>{course.title}</Link>
+              <Link to={'/course/' + course.slug}>{course.title}</Link>
             </td>
             <td>{course.authorName}</td>
             <td>{course.category}</td>
           </tr>
-        ); 
+        );
       })}
     </tbody>
   </table>
