31bac76b8206bf171df614784b91723d4abcceef
David Yaari <david@pronobia.com>
2020-01-27 17:16:22 +0200

    test react

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import * as courseActions from '../../redux/actions/courseActions';
-import * as authorActions from '../../redux/actions/authorActions';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import CourseList from './CourseList';
-import { Redirect } from 'react-router-dom';
-import Spinner from '../common/spinner'
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 
 class CoursesPage extends React.Component {
   state = {
@@ -18,32 +19,49 @@ class CoursesPage extends React.Component {
 
     if (courses.length === 0) {
       actions.loadCourses().catch(error => {
-        alert('Loading courses failed' + error);
+        alert("Loading courses failed" + error);
       });
     }
 
     if (authors.length === 0) {
       actions.loadAuthors().catch(error => {
-        alert('Loading authors failed' + error);
+        alert("Loading authors failed" + error);
       });
     }
   }
 
+  handleDeleteCourse = async course => {
+    toast.success("Course deleted");
+    try {
+      await this.props.actions.deleteCourse(course);
+    } catch (error) {
+      toast.error("Delete failed. " + error.message, { autoClose: false });
+    }
+  };
+
   render() {
     return (
       <>
-        {this.state.redirectToAddCoursePage && <Redirect to='/course' />}
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
-<Spinner />
-        <button
-          style={{ marginBottom: 20 }}
-          className='btn btn-primary add-course'
-          onClick={() => this.setState({ redirectToAddCoursePage: true })}
-        >
-          Add Course
-        </button>
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursePage: true })}
+            >
+              Add Course
+            </button>
 
-        <CourseList courses={this.props.courses} />
+            <CourseList
+              onDeleteClick={this.handleDeleteCourse}
+              courses={this.props.courses}
+            />
+          </>
+        )}
       </>
     );
   }
@@ -52,7 +70,8 @@ class CoursesPage extends React.Component {
 CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 function mapStateToProps(state) {
@@ -66,7 +85,8 @@ function mapStateToProps(state) {
               authorName: state.authors.find(a => a.id === course.authorId).name
             };
           }),
-    authors: state.authors
+    authors: state.authors,
+    loading: state.apiCallsInProgress > 0
   };
 }
 
@@ -74,9 +94,13 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
-      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch)
     }
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
--- src/redux/reducers/apiStatusReducer.js
+++ src/redux/reducers/apiStatusReducer.js
@@ -1,7 +1,7 @@
 import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 
-function actionTypeEndInSuccess(type) {
+function actionTypeEndsInSuccess(type) {
   return type.substring(type.length - 8) === "_SUCCESS";
 }
 
@@ -9,6 +9,14 @@ export default function apiCallStatusReducer(
   state = initialState.apiCallsInProgress,
   action
 ) {
-  if (action.type == types.BEGIN_API_CALL) return (state = state + 1);
+  if (action.type == types.BEGIN_API_CALL) {
+    return state + 1;
+  } else if (
+    action.type === types.API_CALL_ERROR ||
+    actionTypeEndsInSuccess(action.type)
+  ) {
+    return state - 1;
+  }
+
   return state;
 }
